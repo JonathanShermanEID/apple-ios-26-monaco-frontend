91331cc3c223c02ced04a915cc9d9d207a90bdf6
Jonathan Sherman <jonathan@apple-ios26-monaco.com>
2025-10-28 18:13:37 -0400

    Add PWA support for Apple devices - © Jonathan Sherman

---
+++ service-worker.js
@@ -0,0 +1,57 @@
+/**
+ * Service Worker
+ * Apple iOS 26 Monaco Edition
+ * 
+ * Author: Jonathan Sherman
+ * Copyright: © 2025 Jonathan Sherman. All Rights Reserved.
+ * Version: 5.0.0
+ */
+
+const CACHE_NAME = 'apple-ios26-monaco-v5.0.0';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
